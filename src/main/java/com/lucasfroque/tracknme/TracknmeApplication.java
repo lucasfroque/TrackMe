7d3ad899ba7e13edcea9d33af356436da98a7764
lucasfroque <lucasfernandolf38@gmail.com>
2022-05-29 00:55:30 -0300

    First commit

---
+++ src/main/java/com/lucasfroque/tracknme/TracknmeApplication.java
@@ -0,0 +1,13 @@
+package com.lucasfroque.tracknme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracknmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracknmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasfroque/tracknme/TracknmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasfroque.tracknme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracknmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
